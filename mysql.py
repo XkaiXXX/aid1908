b41d33d435ca8c8318824f9558018f1eed67d1f3
XkaiXXX <56906135+XkaiXXX@users.noreply.github.com>
2019-10-23 16:29:24 +0800

    Update mysql.py
    add msg

--- mysql.py
+++ mysql.py
@@ -1,17 +1,5 @@
 
-import  pymysql
-
-#连接数据库
-db=pymysql.connect(host='localhost',#连接的主机地址
-                   port=3306,
-                   user='root',
-                   password='123456',
-                   database='stu',
-                   charset='utf8')
-#生成游标对象(操作数据库  执行sql语句)
-cur=db.cursor()
-
-#执行各种对数据库的读写操作
+print('你好')
 
 
 
